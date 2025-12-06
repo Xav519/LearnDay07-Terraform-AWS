7ab17d40359083af6428f6bc12859e94e884682b
Xav519 <dupuis_x@yahoo.com>
2025-12-06 01:56:51 -0500

    Added securityGroup configurations

---
+++ securityGroup.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.example.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+# Use tuple elements by index:
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = aws_vpc.example.cidr_block
+  from_port         = var.ingress_values[0]
+  ip_protocol       = var.ingress_values[1]
+  to_port           = var.ingress_values[2]
+}
