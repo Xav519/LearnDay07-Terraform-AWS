53ca0aa105b01924cf77ac61c14f98cbfc55b7dc
Xav519 <dupuis_x@yahoo.com>
2025-12-06 02:06:45 -0500

    Added deployment output summary

--- output.tf
+++ output.tf
@@ -7,3 +7,12 @@ output "vpc_name" {
   description = "The ID of the created VPC"
   value       = aws_vpc.example.tags["Name"]
 }
+
+output "deployment_summary" {
+  description = "Summary of the deployment"
+  value = {
+    environment    = var.environment
+    instance_count = var.config.instance_count
+    name_tag = aws_instance.example[0].tags["Name"]
+  }
+}
