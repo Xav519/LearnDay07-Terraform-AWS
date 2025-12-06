bee59d9e069aed9b4cbc172674bcdd225f737fd6
Xav519 <dupuis_x@yahoo.com>
2025-12-06 00:22:01 -0500

    Added output vpc name

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "bucket_name" {
   description = "The name of the created S3 bucket"
   value       = aws_s3_bucket.example.bucket
 }
+
+output "vpc_name" {
+  description = "The ID of the created VPC"
+  value       = aws_vpc.example.tags["Name"]
+}
